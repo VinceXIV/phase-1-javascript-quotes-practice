14aed66a77490320a1851a827b0bd128fcec22f0
Vincent <vincent.makokha@student.moringaschool.com>
2022-08-23 17:06:37 +0300

    Render quotes to the dom

---
+++ src/index.js
@@ -0,0 +1,20 @@
+fetch('http://localhost:3000/quotes?_embed=likes')
+.then(result => result.json())
+.then(data => {
+    const domQuoteList = document.getElementById('quote-list')
+    data.forEach(quote =>{
+        console.log(quote)
+        const domQuoteCard = document.createElement('li')
+        domQuoteCard.classList.add('quote-card')
+        domQuoteCard.innerHTML = `
+            <blockquote class="blockquote">
+            <p class="mb-0">${quote.quote}</p>
+            <footer class="blockquote-footer">${quote.author}</footer>
+            <br>
+            <button class='btn-success'>Likes: <span>0</span></button>
+            <button class='btn-danger'>Delete</button>
+        </blockquote>`
+
+        domQuoteList.append(domQuoteCard)
+    })
+})
